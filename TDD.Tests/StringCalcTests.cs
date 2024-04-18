7c53b2e4215fa124346db9894020771432e3ec0c
Geir Vidar Aasheim <GEVI@equinor.com>
2024-04-18 15:32:10 +0200

    After course. Last task not done.

--- TDD.Tests/StringCalcTests.cs
+++ TDD.Tests/StringCalcTests.cs
@@ -14,5 +14,31 @@ public class StringCalcTests
     public void TestSimpleNumbers()
     {
         Assert.That(calculator.Add("1"), Is.EqualTo(1));
+        Assert.That(calculator.Add("1,4"), Is.EqualTo(5));
+        Assert.That(calculator.Add(""), Is.EqualTo(0));
+        Assert.That(calculator.Add("3,16,200,5"), Is.EqualTo(222));
     }
+
+     [TestCase("1001,1",1)]
+     [TestCase("101",100)]
+     [TestCase("100",99)]
+     [TestCase("99",99)]
+     [TestCase("1000",990)]
+    public void TestOppgave2( string input, int answer)
+    {
+        Assert.That(calculator.Add(input), Is.EqualTo(answer));
+    }    
+    
+    [Test]
+    public void TestOtherSeparators()
+    {
+        Assert.That(calculator.Add("//;\n1;3"), Is.EqualTo(4));
+    }
+
+    [Test]
+    public void TestUtropsTegnBetalerIkkeSkatt()
+    {
+        Assert.That(calculator.Add("1;!999!"), Is.EqualTo(1000));
+    }
+
 }
--- TDD/StringCalc.cs
+++ TDD/StringCalc.cs
@@ -1,10 +1,49 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 namespace TDD;
 
 public class StringCalc
 {
     public int Add(string expression)
     {
-        throw new NotImplementedException();
+       var defaultSeparator  = ',';
+       var formula = expression;
+
+       if (String.IsNullOrEmpty( expression)) 
+       {
+        return 0;
+       }
+
+       if (expression.Contains("//"))
+       {
+          defaultSeparator = expression[2];
+          formula = formula.Substring(4).ToString();
+
+       }
+
+       var s = formula.Split(defaultSeparator).Select(x=> {
+        string finalTextNumber;
+        int finalNumber;
+        bool isTax = false;
+        if(x.Contains("!")){
+            finalTextNumber = x.Substring(1, x.Length-1);
+        }else{
+            finalTextNumber = x;
+            isTax = true;
+        }
+       finalNumber= int.Parse(finalTextNumber);
+
+       if (finalNumber>1000){
+        return 0;
+       }else if(finalNumber > 99&& isTax){
+        return finalNumber - (finalNumber/100);
+       }else{
+        return finalNumber;
+       }
+       } 
+       ).Sum();  
+
+       return s;
     }
 }
